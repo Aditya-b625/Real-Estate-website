83c359aecbe0124555068997d83db4920ce488e7
aditya <adityabhokare93@gmail.com>
2025-10-04 23:18:11 +0530

    sendMail changes

--- backend/middlewares/sendMail.js
+++ backend/middlewares/sendMail.js
@@ -3,7 +3,9 @@ import { createTransport } from "nodemailer";
 const sendMail = async (email, subject, data) => {
   const transport = createTransport({
     host: "smtp.gmail.com",
-    port: 465,
+    // port: 465,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.Gmail,
       pass: process.env.Password,
